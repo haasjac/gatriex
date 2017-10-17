be2a64bf02139d52253b3d4597ab94ef6381fdc8
Jake <haasjac@gmail.com>
2017-10-17 17:29:46 -0400

    email cooper

--- test/index.php
+++ test/index.php
@@ -8,13 +8,27 @@
     //echo $template->render($twig_options);
     
     // The message
-    $message = "Line 1\r\nLine 2\r\nLine 3";
-    
-    // In case any of our lines are larger than 70 characters, we should use wordwrap()
-    $message = wordwrap($message, 70, "\r\n");
+    $message = "Why am I able to do this? wtf.";
     
     // Send
-    $result = mail('haasjac@umich.edu', 'My Subject', $message);
+    //$result = sendEmail('cischken@umich.edu', 'Jake sent this', $message, "Cooper Riehl", "cariehl@umich.edu");
     
     var_dump($result);
+    
+    function sendEmail($mail_to, $mail_subject, $mail_message, $from_name, $from_mail) {
+        //$from_name = "Gatriex";
+        //$from_mail = "DoNotReply@Gatriex.com";
+
+        $encoding = "utf-8";
+
+        // Mail header
+        $header = "Content-type: text/html; charset=".$encoding." \r\n";
+        $header .= "From: ".$from_name." <".$from_mail."> \r\n";
+        $header .= "MIME-Version: 1.0 \r\n";
+        $header .= "Content-Transfer-Encoding: 8bit \r\n";
+        $header .= "Date: ".date("r (T)")." \r\n";
+
+        // Send mail
+        return mail($mail_to, $mail_subject, $mail_message, $header);
+    }
 ?>
