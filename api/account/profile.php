6e36b278cc1429eb7fdf5ed19b64b0f0fcb6d21e
Jake <haasjac@gmail.com>
2017-10-19 14:31:21 -0400

    Riot Test.

--- api/account/profile.php
+++ api/account/profile.php
@@ -5,6 +5,14 @@
     $value = $input->getPost("value");
     $field = $input->getPost("field");
     
+    if ($username === "RiotTest") {
+        $result = new Response();
+        $result->valid = false;
+        $result->data["Error"] = "Sample account. Profile cannot be changed.";
+        echo json_encode($result); 
+        return;
+    }
+    
     $result = $authentication->validateUserFromToken($input->getCookie("Auth_Id"), $input->getCookie("Auth_Token"));
     if (!$result->valid) {
         echo json_encode($result);
